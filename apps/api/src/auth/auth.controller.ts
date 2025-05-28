d4b85201dcb50eb4c62106301ccbcd1522c0bf88
Knight <86920416+ReallyFatYoshi@users.noreply.github.com>
2025-05-28 16:06:26 +0000

    chore: fixed db:generate script inside of the package.json

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
 
 @Controller('auth')
 export class AppController {
